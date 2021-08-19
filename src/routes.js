805858848e43d8d7494137ebf1d653018a0aedd1
srinaththota <srinath.thota@gmail.com>
2021-08-19 17:57:28 +0530

    added routes for login

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 const model = require('./models');
 const routes = require('express').Router();
+const userLogin=require('./controllers/authentication').userLogin
 routes.get('/', (req, res) => {
     res.status(200).json({ message: 'This is Admin Microservice!' });
 });
-
+routes.post('/login', (req, res) => {
+    userLogin(req,res)
+});
 
 module.exports=routes;
